f30c4906f3cba65fb818ada00d1403c6ea954236
Eliot Roxbergh <e@r0x.se>
2020-05-29 18:33:53 +0200

    Added simple script to livestream videos with youtube-dl

---
+++ livestream.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$#" -ne "1" ]; then
+	echo "Enter an URL you'd like to stream"
+	exit 0
+fi
+
+QUALITY="best"
+
+youtube-dl "${1}"  ${QUALITY} -o - 2>/dev/null | vlc -vvv - &>/dev/null
