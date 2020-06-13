7cacf20f559c1d5746c8e21705b952a30909f523
Eliot Roxbergh <eliot@roxbergh.net>
2020-06-13 11:24:01 +0000

    Moved to separate 'example' repo

--- awk_examples.sh
+++
@@ -1,3 +0,0 @@
-# Copy files in current directory which begins with letters (a-z, ignore case) to given path
-# Might be dangerous
-ls -a . | awk '/^[a-zA-Z]/ { print $1 " /your/path/here" }' | xargs -n 2 cp -Ra
